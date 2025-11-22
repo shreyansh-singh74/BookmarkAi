5ff0c76498c6009d46bdcd555fef3e96323a073e
shreyansh <shreyansh.workid@gmail.com>
2025-11-22 15:52:04 +0530

    feat: add dashboard and collection pages with routing

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -1,15 +1,18 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SignIn from "../features/auth/page/Signin";
 import SignUp from "../features/auth/page/Signup";
+import DashboardPage from "../features/dashboard/page/page";
+import CollectionPage from "../features/collections/page/page";
 
 export default function AppRouter(){
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/sign-up" element={<SignUp />} /> 
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/collections" element={<CollectionPage />} />
             </Routes>
         </BrowserRouter>
     )
 }
-
